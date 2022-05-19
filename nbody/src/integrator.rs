bd32b586b5b438f8c07aac78d7f85e09e419fdd0
Matthew Vernacchia <mvernacc@mit.edu>
2022-05-19 00:31:23 -0400

    Add functions to update forces and accelerations on all bodies. The integrator now appears to work -  the Moon moves in the right direction in the example.

--- nbody/src/integrator.rs
+++ nbody/src/integrator.rs
@@ -9,6 +9,10 @@ pub struct LeapfrogIntegrator {
     pub x: Array2<f64>,
     pub v: Array2<f64>,
     pub a: Array2<f64>,
+    /// `forces[i, j, k]` is the gravitational force between bodies `i` and `j` along direction `k`.
+    /// Used internally to avoid repeated calculations; overwritten at each time step.
+    /// [units: N] 
+    forces: Array3<f64>,
 }
 
 impl LeapfrogIntegrator {
@@ -25,10 +29,11 @@ impl LeapfrogIntegrator {
 
         let mut x: Array2<f64> = Array::zeros((n_steps, n_states));
         let mut v: Array2<f64> = Array::zeros((n_steps, n_states));
-        let mut a: Array2<f64> = Array::zeros((n_steps, n_states));
+        let a: Array2<f64> = Array::zeros((n_steps, n_states));
+        let forces: Array3<f64> = Array::zeros((masses.len(), masses.len(), 3));
 
-        x.slice_mut(s![0, ..]).assign(x_init);
-        v.slice_mut(s![0, ..]).assign(v_init);
+        x.slice_mut(s![0usize, ..]).assign(x_init);
+        v.slice_mut(s![0usize, ..]).assign(v_init);
 
         Self {
             dt,
@@ -38,22 +43,25 @@ impl LeapfrogIntegrator {
             x,
             v,
             a,
+            forces,
         }
     }
 
+    /// Integrate the n-body gravitation dynamics through time, using the Leapfrog algorithm.
+    /// The "velocity at integer timesteps" formulation of Leapfrog is used, as given in [Hut2004],
+    /// Ch 4.1, http://www.artcompsci.org/vol_1/v1_web/node34.html, equations 4.4 and 4.5.
+    /// 
+    /// References:
+    ///     [Hut2004] P. Hut and J. Makino, "Moving Stars Around,"" The Art of Computational Science, 2004.
+    ///         http://www.artcompsci.org/vol_1/v1_web/v1_web.html (accessed May 18, 2022).
     pub fn integrate(&mut self) {
         let dt2 = self.dt * self.dt;
         let mut x_current: Array1<f64> = Array::zeros(self.n_states);
         let mut v_current: Array1<f64> = Array::zeros(self.n_states);
-        // `forces[i, j, k]` is the gravitational force between bodies `i` and `j` along direction `k`.
-        // [units: N] 
-        let mut forces: Array3<f64> = Array::zeros((self.n_states / 3, self.n_states / 3, 3));
 
         // Compute accelerations at the initial conditions.
         // TODO
-        self.a
-            .slice_mut(s![0, ..])
-            .assign(&Array::ones(self.n_states));
+        self.update_accelerations(0);
 
         for i in 0..self.n_steps - 1 {
             // Compute positions at the next time step i + 1.
@@ -66,9 +74,7 @@ impl LeapfrogIntegrator {
 
             // Compute accelerations at the next time step i + 1.
             // TODO
-            self.a
-                .slice_mut(s![i + 1, ..])
-                .assign(&Array::ones(self.n_states));
+            self.update_accelerations(i + 1);
 
             // Compute velocities at the next time step i + 1.
             v_current.assign(&self.v.slice(s![i, ..]));
@@ -79,38 +85,66 @@ impl LeapfrogIntegrator {
         }
     }
 
-    fn update_grav_forces_all_bodies(&self, forces: &mut Array3<f64>, x: &Array1<f64>, masses: &Array1<f64>) {
-        let n_bodies = masses.len();
-        assert_eq!(x.len(), 3 * n_bodies);
-        assert_eq!(forces.shape()[0], n_bodies);
-        assert_eq!(forces.shape()[1], n_bodies);
-        assert_eq!(forces.shape()[2], 3);
+    fn update_accelerations(&mut self, step_index: usize) {
+        self.update_grav_forces_all_bodies(step_index);
+
+        let n_bodies = self.masses.len();
+        for i in 0..n_bodies {
+            // Compute the net gravitational force on this body.
+            let mut force_net: [f64; 3] = [0.0, 0.0, 0.0];
+            for j in 0..i {
+                #[allow(clippy::needless_range_loop)]
+                for k in 0..3 {
+                    force_net[k] += self.forces[[i, j, k]];
+                }
+            }
+            for j in i+1..n_bodies {
+                #[allow(clippy::needless_range_loop)]
+                for k in 0..3 {
+                    force_net[k] -= self.forces[[j, i, k]];
+                }
+            }
+
+            // Update the acceleration of this body using F = m a.
+            #[allow(clippy::needless_range_loop)]
+            for k in 0..3 {
+                self.a[[step_index, 3 * i + k]] = force_net[k] / self.masses[i];
+            }
+        }
+    }
+
+    fn update_grav_forces_all_bodies(&mut self, step_index: usize) {
+        let n_bodies = self.masses.len();
+        assert_eq!(self.x.shape()[1], 3 * n_bodies);
+        assert_eq!(self.forces.shape()[0], n_bodies);
+        assert_eq!(self.forces.shape()[1], n_bodies);
+        assert_eq!(self.forces.shape()[2], 3);
 
         let mut r_i: [f64; 3] = Default::default();
         let mut r_j: [f64; 3] = Default::default();
     
         for i in 0..n_bodies {
+            #[allow(clippy::needless_range_loop)]
             for k in 0..3 {
-                r_i[k] = x[3 * i + k];
+                r_i[k] = self.x[[step_index, 3 * i + k]];
             }
             for j in 0..i {
+                #[allow(clippy::needless_range_loop)]
                 for k in 0..3 {
-                    r_j[k] = x[3 * j + k];
+                    r_j[k] = self.x[[step_index, 3 * j + k]];
                 }
-                forces
-                    .slice_mut(s![i, j, ..])
-                    .assign(calc_grav_force_two_bodies(self.masses[i], self.masses[j], r_i, r_j))
+                let f_ij = calc_grav_force_two_bodies(self.masses[i], self.masses[j], r_i, r_j);
+                #[allow(clippy::needless_range_loop)]
+                for k in 0..3 {
+                    self.forces[[i, j, k]] = f_ij[k];
+                }
             }
         }
-    
     }
-
-
 }
 
 /// Gravitational constant [units: N m^2 kg^-1].
 const G: f64 = 6.6743015e-11;
-
 
 
 fn calc_grav_force_two_bodies(mass_1: f64, mass_2: f64, r_1: [f64; 3], r_2: [f64; 3]) -> [f64; 3] {
@@ -121,8 +155,8 @@ fn calc_grav_force_two_bodies(mass_1: f64, mass_2: f64, r_1: [f64; 3], r_2: [f64
     let gmm_over_dist_cubed = G * mass_1 * mass_2 / distance_cubed;
     // Gravitational force of body 2 on body 1 [units: N].
     [
-        gmm_over_dist_cubed * r_1to2[0],
-        gmm_over_dist_cubed * r_1to2[1],
-        gmm_over_dist_cubed * r_1to2[2],
+        -gmm_over_dist_cubed * r_1to2[0],
+        -gmm_over_dist_cubed * r_1to2[1],
+        -gmm_over_dist_cubed * r_1to2[2],
     ]
 }
